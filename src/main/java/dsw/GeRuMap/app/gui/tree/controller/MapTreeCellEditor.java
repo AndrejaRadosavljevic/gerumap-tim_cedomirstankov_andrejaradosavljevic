72c7705430899511c6940a0516b506e5c3c6e7f9
Cedomir <cstankov3921rn@raf.rs>
2022-11-03 12:54:14 +0100

    odradjen controller za tree (MapTreeCellEditor i MapTreeSelectionListener)

--- src/main/java/dsw/GeRuMap/app/gui/tree/controller/MapTreeCellEditor.java
+++ src/main/java/dsw/GeRuMap/app/gui/tree/controller/MapTreeCellEditor.java
@@ -1,5 +1,6 @@
 package dsw.GeRuMap.app.gui.tree.controller;
 
+import dsw.GeRuMap.app.gui.tree.model.MapTreeItem;
 import dsw.GeRuMap.app.gui.tree.view.MapTreeCellRenderer;
 import dsw.GeRuMap.app.gui.tree.view.MapTreeView;
 import lombok.Getter;
@@ -7,55 +8,49 @@ import lombok.Setter;
 
 import javax.swing.*;
 import javax.swing.event.CellEditorListener;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeCellEditor;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.EventObject;
 
 @Getter
 @Setter
 
-public class MapTreeCellEditor implements TreeCellEditor {
+public class MapTreeCellEditor extends DefaultTreeCellEditor implements ActionListener {
 
-    public MapTreeCellEditor(MapTreeView mapTreeView, MapTreeCellRenderer ruTreeCellRenderer) {
+    private Object clickedOn = null;
+    private JTextField edit = null;
+
+    public MapTreeCellEditor(JTree arg0, DefaultTreeCellRenderer arg1){
+        super(arg0,arg1);
+    }
+
+    public Component getTreeCellEditorComponent(JTree arg0, Object arg1, boolean arg2, boolean arg3, boolean arg4, int arg5){
+        //super.getTreeCellEditorComponent(arg0,arg1,arg2,arg3,arg4,arg5);
+        clickedOn=arg1;
+        edit=new JTextField(arg1.toString());
+        edit.addActionListener(this);
+        return edit;
     }
 
     @Override
-    public Component getTreeCellEditorComponent(JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row) {
-        return null;
-    }
-
-    @Override
-    public Object getCellEditorValue() {
-        return null;
-    }
-
-    @Override
-    public boolean isCellEditable(EventObject anEvent) {
+    public boolean isCellEditable(EventObject arg0) {
+        if(arg0 instanceof MouseEvent)
+            if(((MouseEvent)arg0).getClickCount()==3){
+                return true;
+        }
         return false;
     }
 
-    @Override
-    public boolean shouldSelectCell(EventObject anEvent) {
-        return false;
-    }
+    public void actionPerformed(ActionEvent e){
+        if(!(clickedOn instanceof MapTreeItem))
+            return;
 
-    @Override
-    public boolean stopCellEditing() {
-        return false;
-    }
-
-    @Override
-    public void cancelCellEditing() {
-
-    }
-
-    @Override
-    public void addCellEditorListener(CellEditorListener l) {
-
-    }
-
-    @Override
-    public void removeCellEditorListener(CellEditorListener l) {
-
+        MapTreeItem clicked = (MapTreeItem) clickedOn;
+        clicked.setName(e.getActionCommand());
     }
 }

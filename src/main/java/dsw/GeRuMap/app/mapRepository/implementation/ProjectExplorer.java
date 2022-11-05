48f833145db6281339afef623724584dcb2dbfe6
Cedomir <cstankov3921rn@raf.rs>
2022-11-05 13:36:21 +0100

    dodate jos neke stvari u ProjectExplorer-u i U MapNodeComposite-u

--- src/main/java/dsw/GeRuMap/app/mapRepository/implementation/ProjectExplorer.java
+++ src/main/java/dsw/GeRuMap/app/mapRepository/implementation/ProjectExplorer.java
@@ -13,4 +13,13 @@ public class ProjectExplorer extends MapNodeComposite {
     public ProjectExplorer(String name) {
         super(name,null);
     }
+    @Override
+    public void addChild(MapNode child) {
+        if (child != null &&  child instanceof Project){
+            Project project = (Project) child;
+            if (!this.getChildren().contains(project)){
+                this.getChildren().add(project);
+            }
+        }
+    }
 }

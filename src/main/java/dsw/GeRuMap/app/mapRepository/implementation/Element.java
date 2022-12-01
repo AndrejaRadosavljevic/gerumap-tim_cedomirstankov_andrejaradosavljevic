fb7ffd62f3d115603246b1ed6ca185a49e12f954
Cedomir <cstankov3921rn@raf.rs>
2022-12-01 17:21:56 +0100

    dodato je par paintera i diagramdevice.

--- src/main/java/dsw/GeRuMap/app/mapRepository/implementation/Element.java
+++ src/main/java/dsw/GeRuMap/app/mapRepository/implementation/Element.java
@@ -1,17 +1,29 @@
 package dsw.GeRuMap.app.mapRepository.implementation;
 
 import dsw.GeRuMap.app.mapRepository.composite.MapNode;
+import dsw.GeRuMap.app.painters.ElementPainter;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.awt.*;
 
 @Getter
 @Setter
 public class Element extends MapNode {
-    private int x, y;
-    private int width, length;
 
-    private int lineWidth;
-    private int color;
+    private Stroke stroke;
+    private Paint paint;
+
+    //private String name;
+    //private String description;
+
+    private ElementPainter elementPainter;
+
+    public Element(String name, MapNode parent,Stroke stroke, Paint paint){
+        super(name,parent);
+        this.stroke = stroke;
+        this.paint = paint;
+    }
 
     public Element(String name, MapNode parent) {
         super(name, parent);
---
+++ src/main/java/dsw/GeRuMap/app/mapRepository/implementation/elements/DiagramDevice.java
@@ -0,0 +1,42 @@
+package dsw.GeRuMap.app.mapRepository.implementation.elements;
+
+import dsw.GeRuMap.app.mapRepository.composite.MapNode;
+import dsw.GeRuMap.app.mapRepository.implementation.Element;
+
+import java.awt.*;
+
+public abstract class DiagramDevice extends Element {
+
+    protected Dimension size;
+    protected Point position;
+
+
+
+
+    public DiagramDevice(String name, MapNode parent, Point position, Dimension size, Stroke stroke, Paint paint){
+        super(name,parent,stroke, paint);
+        this.size = size;
+        this.position = position;
+    }
+
+
+
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public Dimension getSize() {
+        return size;
+    }
+
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
+
+
+}

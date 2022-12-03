0377ab81c87ca4af9e4e11bde2698b34e6fe0116
Andreja Radosavljevic <aradosavljevic3521rn@raf.rs>
2022-12-03 15:36:36 +0100

    Popravljen extend kod VezaPainter

---
+++ src/main/java/dsw/GeRuMap/app/painters/PojamPainter.java
@@ -0,0 +1,13 @@
+package dsw.GeRuMap.app.painters;
+
+import dsw.GeRuMap.app.mapRepository.implementation.Element;
+import dsw.GeRuMap.app.mapRepository.implementation.elements.PojamElement;
+
+public class PojamPainter extends DevicePainter{
+
+    private PojamElement element;
+
+    public PojamPainter(Element device) {
+        super(device);
+    }
+}

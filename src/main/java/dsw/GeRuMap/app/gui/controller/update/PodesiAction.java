3520200c1a8561069b6535bb7332923494878e76
Andreja Radosavljevic <aradosavljevic3521rn@raf.rs>
2022-12-03 19:34:34 +0100

    MapView fali zbog nekog razloga

---
+++ src/main/java/dsw/GeRuMap/app/gui/controller/update/PodesiAction.java
@@ -0,0 +1,18 @@
+package dsw.GeRuMap.app.gui.controller.update;
+
+import dsw.GeRuMap.app.gui.controller.AbstractGeRuMapAction;
+import dsw.GeRuMap.app.gui.view.MainFrame;
+import dsw.GeRuMap.app.gui.view.MapTab;
+import dsw.GeRuMap.app.gui.view.NewPojamFrame;
+import dsw.GeRuMap.app.mapRepository.implementation.elements.PojamElement;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class PodesiAction extends AbstractGeRuMapAction {
+
+    public void actionPerformed(ActionEvent arg0){
+
+
+    }
+}

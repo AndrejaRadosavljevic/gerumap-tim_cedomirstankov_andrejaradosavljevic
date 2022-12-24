7e74e11825127e73ea5216eef8b18516135bafe7
Andreja Radosavljevic <aradosavljevic3521rn@raf.rs>
2022-12-24 14:05:48 +0100

    Zapoceta izrada otvaranja projekta uz pomoc serializer-a

--- src/main/java/dsw/GeRuMap/app/core/Serializer.java
+++ src/main/java/dsw/GeRuMap/app/core/Serializer.java
@@ -1,13 +1,18 @@
 package dsw.GeRuMap.app.core;
 
-import dsw.GeRuMap.app.gui.view.MapTab;
+import com.google.gson.JsonArray;
+import dsw.GeRuMap.app.mapRepository.composite.MapNode;
 import dsw.GeRuMap.app.mapRepository.implementation.MindMap;
 import dsw.GeRuMap.app.mapRepository.implementation.Project;
 
 import java.io.File;
+import java.util.List;
 
 public interface Serializer {
     Project loadProject(File file);
+    List<MapNode> loadMindMaps(JsonArray jsonMaps);
+    List<MapNode> loadElements(JsonArray jsonElements);
+
     void saveProject(Project node);
     void saveMap(MindMap node);
     void saveTemplate(MindMap node);
--- src/main/java/dsw/GeRuMap/app/serializer/GsonSerializer.java
+++ src/main/java/dsw/GeRuMap/app/serializer/GsonSerializer.java
@@ -1,11 +1,16 @@
 package dsw.GeRuMap.app.serializer;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import dsw.GeRuMap.app.core.Serializer;
+import dsw.GeRuMap.app.mapRepository.composite.MapNode;
 import dsw.GeRuMap.app.mapRepository.implementation.MindMap;
 import dsw.GeRuMap.app.mapRepository.implementation.Project;
+import dsw.GeRuMap.app.mapRepository.implementation.elements.PojamElement;
+import dsw.GeRuMap.app.mapRepository.implementation.elements.VezaElement;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GsonSerializer implements Serializer {
 
@@ -14,7 +19,11 @@ public class GsonSerializer implements Serializer {
     @Override
     public Project loadProject(File file) {
         try (FileReader fileReader = new FileReader(file)) {
-            return gson.fromJson(fileReader, Project.class);
+            FileReader reader = new FileReader(file);
+            Project project =  gson.fromJson(fileReader, Project.class);
+            JsonObject object = JsonParser.parseReader(reader).getAsJsonObject();
+            project.setChildren(loadMindMaps((JsonArray) object.get("children")));
+            return project;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -22,21 +31,52 @@ public class GsonSerializer implements Serializer {
     }
 
     @Override
-    public void saveProject(Project node) {
-        try (FileWriter writer = new FileWriter(node.getPath())) {
-            gson.toJson(node, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public List<MapNode> loadMindMaps(JsonArray jsonMaps) {
+        List<MapNode> maps = new ArrayList<>();
+        for (JsonElement map : jsonMaps) {
+            JsonObject m = map.getAsJsonObject();
+            MindMap mindMap = gson.fromJson(map.toString(), MindMap.class);
+
+            JsonArray elements = (JsonArray) m.get("children");
+
+            List<MapNode> e = loadElements(elements);
+
+            mindMap.setChildren(e);
         }
+        return maps;
     }
+        @Override
+        public List<MapNode> loadElements(JsonArray jsonElements) {
+            List<MapNode> elements = new ArrayList<>();
+            for(JsonElement element:jsonElements){
+                if(element.getAsJsonObject().has("pE1")){
+                    elements.add(gson.fromJson(element.toString(), VezaElement.class));
+                } else {
+                    elements.add(gson.fromJson(element.toString(), PojamElement.class));
+                }
+            }
 
-    @Override
-    public void saveMap(MindMap node) {
+            return elements;
+        }
 
-    }
 
-    @Override
-    public void saveTemplate(MindMap node) {
+        @Override
+        public void saveProject(Project node) {
+            try (FileWriter writer = new FileWriter(node.getPath())) {
+                gson.toJson(node, writer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-    }
+        @Override
+        public void saveMap(MindMap node) {
+
+        }
+
+        @Override
+        public void saveTemplate(MindMap node) {
+
+        }
+
 }
